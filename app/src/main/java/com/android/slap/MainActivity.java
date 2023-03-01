27b41e3c40fa950108c6ac93eee2b4b9ec518242
hieu.nq <ngohieu1811@gmail.com>
2023-03-01 20:19:03 +0700

    done

--- app/src/main/java/com/android/slap/MainActivity.java
+++ app/src/main/java/com/android/slap/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    public static boolean THAY_TUAN = true;
+    public static boolean THAY_TUAN = false;
     public static String USER_ID = "20182514";
     public static Map<String, SinhVienDAO> MAP_STUDENT = new HashMap<>();
 
