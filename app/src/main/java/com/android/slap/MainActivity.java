2bead0bbd879d618cef57cf0ead0a34e86abada3
Nguyễn Thành Nam <65747524+boyit1309@users.noreply.github.com>
2023-05-24 09:44:07 +0700

    Update logic màn main

--- app/src/main/java/com/android/slap/MainActivity.java
+++ app/src/main/java/com/android/slap/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     public static boolean THAY = false;
-    public static String USER_ID = "123456";
+    public static String USER_ID = "123456";                 
     public static Map<String, SinhVienDAO> MAP_STUDENT = new HashMap<>();
 
     @Override
